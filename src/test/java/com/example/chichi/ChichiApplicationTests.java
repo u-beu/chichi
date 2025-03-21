f975394de7992ba0aa2ae5ee6970a8bab00a9bd3
u-beu <uzbn22@gmail.com>
2025-03-21 10:45:30 +0900

    초기 설정

---
+++ src/test/java/com/example/chichi/ChichiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
