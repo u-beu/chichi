39f63b26162b9fe94a8218d276d7e5417566387f
u-beu <uzbn22@gmail.com>
2025-04-01 14:03:38 +0900

    회원가입과 패스워드 변경 요청에 유효성 검사 추가 및 예외 핸들러 구현

--- src/main/java/com/example/chichi/domain/user/dto/JoinUserRequest.java
+++ src/main/java/com/example/chichi/domain/user/dto/JoinUserRequest.java
@@ -3,18 +3,15 @@ package com.example.chichi.domain.user.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record ChangePasswordRequest(
+public record JoinUserRequest(
+        @NotNull
+        @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$",
+                message = "이메일 양식을 확인해주세요.")
+        String email,
         @NotNull
         @Pattern(
                 regexp = "^\\d{6}$",
                 message = "패스워드는 숫자 6자리여야 합니다."
         )
-        String currentPassword,
-        @NotNull
-        @Pattern(
-                regexp = "^\\d{6}$",
-                message = "패스워드는 숫자 6자리여야 합니다."
-        )
-        String newPassword
-) {
+        String password) {
 }
--- src/main/java/com/example/chichi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/chichi/exception/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.example.chichi.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+import static com.example.chichi.exception.ExceptionType.SERVER_ERROR;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -12,14 +20,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<String> handleApiException(ApiException e) {
-        return ResponseEntity.status(e.getStatusCode())
-                .body(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatusCode()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         log.debug("예외 [{}]:{}", e.getClass().getSimpleName(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("서버 내부 오류");
+        return new ResponseEntity<>(SERVER_ERROR.getMessage(), SERVER_ERROR.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String errorMessage = bindingResult.getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("\n"));
+        return new ResponseEntity<>(errorMessage, BAD_REQUEST);
     }
 }
