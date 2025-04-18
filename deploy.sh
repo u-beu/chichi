ec11404382df7b7124c14cb95867b5058799270a
u-beu <uzbn22@gmail.com>
2025-04-18 16:41:05 +0900

    deply.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source .env
+echo "현재 ACTIVE_COLOR=$ACTIVE_COLOR"
 if [ "$ACTIVE_COLOR" = "blue" ]; then
     NEW_COLOR=green
     OLD_COLOR=blue
@@ -11,15 +12,15 @@ else
     NEW_PORT=8081
 fi
 
-echo "(app-$NEW_COLOR) 실행"
-docker compose up -d app-$NEW_COLOR
+echo "app-$NEW_COLOR 실행"
+sudo docker compose up -d app-$NEW_COLOR
 
 echo "대기"
 sleep 5
 
 echo "ACTIVE_COLOR=$NEW_COLOR" > .env
-docker compose restart nginx
+sudo docker compose restart nginx
 
-echo "(app-$OLD_COLOR) 정지 및 삭제"
-docker compose stop app-$OLD_COLOR
-docker compose rm -f app-$OLD_COLOR
+echo "app-$OLD_COLOR 정지 및 삭제"
+sudo docker compose stop app-$OLD_COLOR
+sudo docker compose rm -f app-$OLD_COLOR
